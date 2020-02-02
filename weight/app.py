cdb4d782628c35ef226ac947fea5a0e5682013da
Oz Shmuel <ozshmuel@gmail.com>
2020-02-02 14:54:16 +0200

    update app.py

--- weight/app.py
+++ weight/app.py
@@ -6,13 +6,13 @@ from mysql.connector import Error
 app = Flask(__name__)
 
 
-def check_db_status(host='localhost',database='Electronics',user='root',password='123'):
-
+def check_db_status(host='db',database='weightDB',user='root',password='alpine'):
+    print("trying to connect...")
     #print("host:{}\n db:{}\n user:{}\n pass:{}".format(host,database,user,password))
-    '''
+    
     try:
         connection = mysql.connector.connect(host=host,
-                                         database=database, #Example DB = 'Electronics'
+                                         database=database, 
                                          user=user,
                                          password=password)
 
@@ -21,17 +21,24 @@ def check_db_status(host='localhost',database='Electronics',user='root',password
             return True
     except:
         return False
-    '''
+    
     return True
 
 @app.route("/")
 @app.route("/health")
 def health():
+
+    '''
+    print("enter to health\n")
     if (check_db_status()):
         return "OK" #response status = 200 'OK'
     
     else:
         return Response(status=400)
+
+    '''
+    print("Testing....")
+    return "OK!!!"
 
 
 if __name__ == '__main__':
