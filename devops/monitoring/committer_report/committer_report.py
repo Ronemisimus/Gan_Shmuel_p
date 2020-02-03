747c1c0529c167a6e48a83b0c256b3aa00ff6c0d
vdhyvtcakthdur <eigorek@gmail.com>
2020-02-03 13:15:23 +0200

    added another email change

--- devops/monitoring/committer_report/committer_report.py
+++ devops/monitoring/committer_report/committer_report.py
@@ -25,10 +25,10 @@ mail = Mail(app)
 @app.route('/committer_report', methods=['POST'])
 def log():
    data = request.get_json()
-   user_name =  data['repository']['name']
+   comment = data['comment']
 
    # data = json.loads(request.data)
-   msg_to_send = "New commit by: {}".format(user_name)
+   msg_to_send = "New commit by: {}".format(comment)
    msg = Message(msg_to_send, sender="gan.shmuel.ashdod@gmail.com",  recipients=["eigorek@gmail.com"])
    mail.send(msg)
    return "ok"
