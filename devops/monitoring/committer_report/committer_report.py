6e60894d8df04827edbdbfae1c13932098e6f31c
vdhyvtcakthdur <eigorek@gmail.com>
2020-02-03 17:24:22 +0200

    another test email

--- devops/monitoring/committer_report/committer_report.py
+++ devops/monitoring/committer_report/committer_report.py
@@ -9,7 +9,7 @@ app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USERNAME'] = "gan.shmuel.ashdod@gmail.com"
 app.config['MAIL_PASSWORD'] = "nA@UemKJPlo0GjperLN4"
-#
+
 mail = Mail(app)
 
 @app.route('/health', methods=['GET'])
