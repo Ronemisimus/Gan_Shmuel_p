4f81d43014aa979727140684732a3ee611f519ce
nati-elmaliach <natielmaliach3197@gmail.com>
2020-02-03 02:05:39 +0200

    Formating the code and adding url enterypoints

--- weight/app.py
+++ weight/app.py
@@ -1,41 +1,51 @@
 from flask import Flask, Request, Response
+from flask import request
 import mysql.connector
 from mysql.connector import Error
-
-
+from insertions import read_json_file , read_csv_file
 app = Flask(__name__)
 
 
+# Todo: See how we can instantiate the DB only once , and pass it to app.py
 def check_db_status(host='db',database='weightDB',user='user',password='alpine'):
-    print("trying to connect...")
-    #print("host:{}\n db:{}\n user:{}\n pass:{}".format(host,database,user,password))
-    
     return mysql.connector.connect(password='alpine', user='root', host='db', port='3306', database='weightDB' ,  auth_plugin='mysql_native_password')
 
 
+# Entery points
 @app.route("/")
 @app.route("/health")
 def health():
-
     if check_db_status():
         return Response(status=200)
     else:
         return Response(status=500)
-    
-    # Todo:
-    # select 1 from db 
-    '''
-    print("enter to health\n")
-    if (check_db_status()):
-        return "OK" #response status = 200 'OK'
-    
-    else:
-        return Response(status=400)
 
-    '''
-    print("Testing....")
-    return "OK!!!"
+@app.route('/batch-weight-file<string:filename>' , methods=["POST"])
+def batch_weight(filename):
+    # Will upload list of tara weights from a file in "/in" folder. Usually used to accept a batch of new containers. 
+    # File formats accepted: csv (id,kg), csv (id,lbs), json ([{"id":..,"weight":..,"unit":..},...])
+    return "OK"
 
+@app.route('/unknown' , methods=["GET"])
+def unknown():
+    # Returns a list of all recorded containers that have unknown weight:
+    # ["id1","id2",...]
+    return "OK"
+
+@app.route('/session/<id>' , methods=["GET"])
+def session(id):
+    # Read the instructions
+    return "OK"
+
+@app.route('/weight' , methods=["GET"])
+def get_weight():
+    # Read the instructions
+    return "OK"
+
+@app.route('/item/<id>' , methods=["GET"])
+def get_item(id):
+    # Read the instructions
+    return "OK"
 
 if __name__ == '__main__':
     app.run(debug = True, host="0.0.0.0")
--- weight/insertions.py
+++ weight/insertions.py
@@ -1,25 +1,17 @@
-import mysql.connector
 import csv
 import sys
 import json
-from mysql.connector import Error
 
-def usage():
-    print("""\
-        Runing app:
-        insertions.py <path to file>
-        Please ensure that file path correct and file exist   
-    """)
-    exit()
-
+# Read file in json format
 def read_json_file(filename):
+    return True
     with open(filename, 'r') as json_file:
         data = json.load(json_file)
         for line in data:
             # print (line.get("id") + " weight " + str(line.get("weight")) + " units " + line.get("unit"))
             insertVariblesIntoTable(line.get("id"), line.get("weight"), line.get("unit"))
 
-
+# Read file in csv format
 def read_csv_file(filename):
     with open(filepath, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
@@ -28,44 +20,3 @@ def read_csv_file(filename):
         for line in csv_reader:
             insertVariblesIntoTable(line[0], int(line[1]), unit)
 
-
-def insertVariblesIntoTable(cont_id, weight, unit):
-    try:
-        print("insert to table values: c_id " + cont_id + "  weight   " + str(weight) + "  unit " + unit)
-    #     connection = mysql.connector.connect(host='localhost:8081',
-    #                                          database='weightDB',
-    #                                          user='user',
-    #                                          password='alpine')
-    #     cursor = connection.cursor()
-    #     mySql_insert_query = """INSERT INTO containers (cont_id, weight, unit) 
-    #                             VALUES (%s, %s, %s) """
-
-    #     recordTuple = (cont_id, weight, unit)
-    #     cursor.execute(mySql_insert_query, recordTuple)
-    #     connection.commit()
-    #     print("Record inserted successfully into containers table")
-
-    except mysql.connector.Error as error:
-        print("Failed to insert into MySQL table {}".format(error))
-
-    # finally:
-    #     if (connection.is_connected()):
-    #         cursor.close()
-    #         connection.close()
-    #         print("MySQL connection is closed")
-
-
-# main 
-if(len(sys.argv) !=  2):
-    usage()
-else:
-    filepath = sys.argv[1]
-    try:
-        if ".csv" in filepath:
-            read_csv_file(filepath)
-        if ".json" in filepath:
-            read_json_file(filepath)
-    except:
-        usage()
-
-print("Insertion is done")
