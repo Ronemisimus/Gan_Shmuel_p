ecb60062883aab27f01064680b719140d5d30f67
Itzik Alayev <startukk@gmail.com>
2020-02-04 22:55:18 +0200

    Added CI to production environment

--- devops/ci-server/app.py
+++ devops/ci-server/app.py
@@ -7,20 +7,31 @@ from time import sleep
 app = Flask(__name__)
 
 WORKDIR = '/ci-server'
-REPOSITORY_URL = 'https://github.com/ChrisPushkin/Gan_Shmuel_p.git'
+#REPOSITORY_URL = 'https://github.com/ChrisPushkin/Gan_Shmuel_p.git'
+REPOSITORY_URL = 'https://github.com/itzik-alayev/Gan_Shmuel_p'
 TESTING_DIR = 'test/'
+STAGE_DIR = 'stage/'
 PRODUCTION_DIR = 'production/'
+TEST_PORT = "8082"
 
 def find(name, path):
     for root, dirs, files in os.walk(path):
         if name in files:
             return os.path.join(root, name)
 
+def find_all(name, path):
+    result = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root, name))
+    return result
+
 
 @app.route('/payload', methods=['POST'])
 def gitWebHook():
 	data = request.get_json()
 	branch = data['ref'].split('/')[-1]
+	main_folder = os.getcwd()
 
 	os.system('rm -rf {}{}'.format(TESTING_DIR, branch))
 	os.system('git clone {} --single-branch -b {} {}{}'.format(REPOSITORY_URL, branch, TESTING_DIR, branch))
@@ -36,8 +47,6 @@ def gitWebHook():
 	'''
 
 	if branch == 'weight' or branch == 'provider':
-		environment = 'test'
-
 		# Finding the Dockerfile
 		docker_file = find('Dockerfile', '{}{}'.format(TESTING_DIR, branch))
 		docker_path = '/'.join(docker_file.split('/')[:-1])
@@ -48,40 +57,70 @@ def gitWebHook():
 
 		# Load .env file as environment variables
 		load_dotenv('{}/.env'.format(compose_path), override=True)
+		stage_port = os.environ['PORT']
 
 		# Build Dockerfile to get image artifact
 		os.system('docker build -t {} ./{}'.format(branch, docker_path))
 
-		# Run the test build
+		# Start testing environment
 		os.chdir(compose_path)
+		environment = 'test'
+		os.environ['PORT'] = TEST_PORT
 		os.system('docker-compose -p {}-{} up -d'.format(environment, branch))
-		os.chdir(WORKDIR)
+		
+		# Tests go here
+		###############
+		###############
+		###############
 
-		# MAIN TODO:
-		# End-2-End testing
+		os.system('docker-compose -p {}-{} stop'.format(environment, branch))
+		os.system('docker-compose -p {}-{} rm -f'.format(environment, branch))
+
+		# Test was successful, run the tested image in stage environment
+		# and move its source code to stage/ folder
+		environment = 'stage'
+		os.environ['PORT'] = stage_port
+		os.system('docker-compose -p {}-{} up -d'.format(environment, branch))
+
+		os.chdir(main_folder)
+		os.system('rm -rf {}{} && cp -R {}/ {}{}'.format(STAGE_DIR, branch, compose_path, STAGE_DIR, branch))
 	
 	if branch == 'master':
-		environment = 'prod'
+		compose_files = find_all('docker-compose.yml', '{}{}'.format(TESTING_DIR, branch))
+		compose_paths = [('/').join(file.split('/')[:-1]) for file in compose_files]
 
-		# TODO: build all folders in master branch
-		# 		test them and run as production
+		for path in compose_paths:
+			environment = 'test'
+			if path.find('devops') != -1:
+				continue
 
-	'''
-	# WILL BE USED LATER
+			load_dotenv(dotenv_path='{}/.env'.format(path), override=True)
+			app_name = os.environ['IMAGE_NAME']
+			prod_port = os.environ['PROD_PORT']
 
-	# Test complete, remove test containers and update production containers
-	# ..to work with new images.
-	#os.system('docker-compose -f {} kill'.format(compose_file))
-	
-	# Copy successful test clone to staging
-	os.system('rm -rf {}{}'.format(PRODUCTION_DIR, compose_path))
-	os.system('cp -rf {} {}'.format(compose_path, PRODUCTION_DIR))
+			# Build app image
+			os.system('docker build -t {} ./{}'.format(app_name, '/'.join(find('Dockerfile', path).split('/')[:-1])))
 
-	# Run app with production port
-	os.environ['PORT'] = prod_port
-	os.system('docker-compose -f {}{}/docker-compose.yml -p prod-{} up -d'.format(PRODUCTION_DIR, compose_path.split('/')[-1], os.environ['IMAGE_NAME']))
-	'''
+			# Run test docker-compose
+			os.chdir(path)
+			os.environ['PORT'] = TEST_PORT
+			os.system('docker-compose -p {}-{} up -d'.format(environment, app_name))
+
+			# Tests go here
+			###############
+			###############
+			###############
+
+			os.system('docker-compose -p {}-{} stop'.format(environment, app_name))
+			os.system('docker-compose -p {}-{} rm -f'.format(environment, app_name))
+
+			# Tests passed, run production
+			environment = 'prod'
+			os.environ['PORT'] = prod_port
+			os.system('docker-compose -p {}-{} up -d'.format(environment, app_name))
+			os.chdir(main_folder)
+			os.system('rm -rf {}{} && cp -R {}/ {}{}'.format(PRODUCTION_DIR, app_name, path, PRODUCTION_DIR, app_name))
 	return Response(status=200)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8085, threaded=True, debug=True)
+    app.run(host='0.0.0.0', port=8085, debug=True)
