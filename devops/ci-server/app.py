36e0d7fde8dc1c7993a35524384b04bbf1fb5bc9
Itzik Alayev <startukk@gmail.com>
2020-02-04 12:16:17 +0200

    ci changed

--- devops/ci-server/app.py
+++ devops/ci-server/app.py
@@ -51,7 +51,7 @@ def gitWebHook():
 		os.system('docker build -t {} ./{}'.format(branch, docker_path))
 
 		# Run the test build
-		print('PWD: {}'.format(os.environ['PWD']), file=sys.stderr)
+		#print('PWD: {}'.format(os.environ['PWD']), file=sys.stderr)
 		os.system('docker-compose -f {} config')
 		os.system('docker-compose -f {} -p {}-{} up -d'.format(compose_file, environment, branch))
 
