e38188267148a5aee7e8db2b578247becdf868e2
Itzik Alayev <startukk@gmail.com>
2020-02-04 13:45:40 +0200

    Changed app.py

--- devops/ci-server/app.py
+++ devops/ci-server/app.py
@@ -55,7 +55,10 @@ def gitWebHook():
 		print('docker path: {}'.format(docker_path), file=sys.stderr)
 		print('compose file: {}'.format(compose_file), file=sys.stderr)
 		print('compose_path: {}'.format(compose_path), file=sys.stderr)
-		os.system('sudo {} docker-compose -p {}-{} up -d'.format(compose_path, environment, branch))
+		os.chdir(compose_path)
+		os.system('ls -la')
+		os.system('docker-compose -p {}-{} up -d'.format(environment, branch))
+		os.chdir('/ci-test')
 
 		# MAIN TODO:
 		# End-2-End testing
