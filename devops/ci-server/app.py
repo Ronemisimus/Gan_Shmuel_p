0aaca2d5bdb7608982dd8cc618cd1ddc50ebcfdb
Itzik Alayev <startukk@gmail.com>
2020-02-04 13:21:53 +0200

    Trying to fix db volumes

--- devops/ci-server/app.py
+++ devops/ci-server/app.py
@@ -57,9 +57,9 @@ def gitWebHook():
 		print('docker path: {}'.format(docker_path), file=sys.stderr)
 		print('compose file: {}'.format(compose_file), file=sys.stderr)
 		print('compose_path: {}'.format(compose_path), file=sys.stderr)
-		os.system('cd {}'.format(compose_path))
-		os.system('docker-compose -p {}-{} up -d'.format(compose_file, environment, branch))
-		os.system('cd -')
+		os.system('pushd {}'.format(compose_path))
+		os.system('docker-compose -p {}-{} up -d'.format(environment, branch))
+		os.system('popd')
 
 		# MAIN TODO:
 		# End-2-End testing
