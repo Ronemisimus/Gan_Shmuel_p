bfed0eaa51d8d565b82435547b7fc286564a3fd3
RonBenMoshe <ronmoshe333@gmail.com>
2020-02-04 16:37:40 +0200

    Od MAshu

---
+++ providers/db.sql
@@ -0,0 +1,109 @@
+-- MySQL dump 10.13  Distrib 5.7.29, for Linux (x86_64)
+--
+-- Host: localhost    Database: billdb
+-- ------------------------------------------------------
+-- Server version	5.7.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `billdb`
+--
+
+
+USE `billdb`;
+
+--
+-- Table structure for table `Provider`
+--
+
+DROP TABLE IF EXISTS `Provider`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Provider` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=MyISAM AUTO_INCREMENT=10004 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Provider`
+--
+
+LOCK TABLES `Provider` WRITE;
+/*!40000 ALTER TABLE `Provider` DISABLE KEYS */;
+INSERT INTO `Provider` VALUES (10001,'ALL'),(10002,'pro1'),(10003,'pro2');
+/*!40000 ALTER TABLE `Provider` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Rates`
+--
+
+DROP TABLE IF EXISTS `Rates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Rates` (
+  `product_id` varchar(50) NOT NULL,
+  `rate` int(11) DEFAULT '0',
+  `scope` varchar(50) NOT NULL DEFAULT 'ALL',
+  PRIMARY KEY (`product_id`,`scope`),
+  KEY `scope` (`scope`)
+) ENGINE=MyISAM AUTO_INCREMENT=10001 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Rates`
+--
+
+LOCK TABLES `Rates` WRITE;
+/*!40000 ALTER TABLE `Rates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Rates` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `Trucks`
+--
+
+DROP TABLE IF EXISTS `Trucks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Trucks` (
+  `id` varchar(10) NOT NULL,
+  `provider_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `provider_id` (`provider_id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Trucks`
+--
+
+LOCK TABLES `Trucks` WRITE;
+/*!40000 ALTER TABLE `Trucks` DISABLE KEYS */;
+INSERT INTO `Trucks` VALUES ('134-33-443',2),('222-33-111',1);
+/*!40000 ALTER TABLE `Trucks` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2020-02-04 14:33:57
