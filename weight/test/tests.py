9ba1f49750755d2430c952265a22deb9456aa78c
nati-elmaliach <natielmaliach3197@gmail.com>
2020-02-05 23:58:04 +0200

    testing functions

--- weight/test/tests.py
+++ weight/test/tests.py
@@ -121,6 +121,7 @@ def test_get_routes(path , expected):
 test = False
 def main():
     test_health()
+    
     test_get_routes('/unknown' ,{"7":{"ContainerID":"C1","Produce":"Test","TransactionID":"36"}})
     test_get_routes('/item/Truck1?from=20200202112732&to=20211231011500' ,{"id":"Truck1","sessions":[35],"tara":92})
     test_get_routes('/session/35' , {"id": "35","truckID": "Truck1","items": [{"produce": "Oranges", "bruto" : "46", "neto": "null"},{"produce": "Apples", "bruto" : "76", "neto": "null"}]})
