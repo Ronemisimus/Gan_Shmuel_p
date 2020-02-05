2db5ce75edc5350e6e5b69278f25092989e7238f
nati-elmaliach <natielmaliach3197@gmail.com>
2020-02-05 19:25:16 +0200

    sessin print

--- weight/test/tests.py
+++ weight/test/tests.py
@@ -61,6 +61,7 @@ def test_weight_route( path, expected):
         status = 1
     else:
         expected_res = json.dumps(expected)
+        print(res)
         status = 0 if expected_res == res else 1
         
     
