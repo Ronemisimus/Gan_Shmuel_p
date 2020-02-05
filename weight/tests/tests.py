fab18d85f9e25bb58c837af1d1b514ce7ce02ad9
Oz Shmuel <ozshmuel@gmail.com>
2020-02-05 15:50:05 +0200

    test /unknown and /health were added

--- weight/tests/tests.py
+++ weight/tests/tests.py
@@ -21,9 +21,6 @@ def validate(status, content, expected):
 
 
 
-
-
-
 def test__get_routes(route, expected):
     res = requests.get(route)
     log , code  = validate(int(res.status_code), str(res.content), expected)
@@ -42,8 +39,11 @@ def test__post_routes():
 
 def main():
     # test__get_routes(url+"/weight?from=10000303000000&to=30000303000000&f=in",'{')
-    test__get_routes(url+"/weight?from=10000303000000&to=30000303000000",'{"35":{"bruto":"178","containers":"C1,C2","direction":"in","neto":"92","produces":"Apples,Oranges"},"36":{"bruto":"125","containers":"C1","direction":"In","neto":"12","produces":"Test,Tomato"}}')
-
+    #test__get_routes(url+"/weight?from=10000303000000&to=30000303000000",'{"35":{"bruto":"178","containers":"C1,C2","direction":"in","neto":"92","produces":"Apples,Oranges"},"36":{"bruto":"125","containers":"C1","direction":"In","neto":"12","produces":"Test,Tomato"}}')
+    #testing /health route
+    test__get_routes(url+"/health", "b'OK'")
+    #testing /unknown route
+    test__get_routes(url+"/unknown", "b'7  '")
 
     # test_rout_weight(url+"/weight",{})
 
