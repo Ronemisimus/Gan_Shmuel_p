3be936b68d1c0957cf66147c71ec86fc17592cdf
Aran Shavit <aranshavit@gmail.com>
2020-02-05 20:05:33 +0200

    added /weight GET and POST tests

--- weight/test/tests.py
+++ weight/test/tests.py
@@ -81,6 +81,34 @@ def test_item_route( path, expected):
     if res != expected_res:
         status = 1
 
+def test_weightPost_route( path, expected):
+    global url
+    global status
+    res =''
+    expected_res = ''
+    try:
+        res = str(requests.post(url + path))
+        expected_res = expected
+    except Exception as e:
+        status = 1
+
+    if res != expected_res:
+        status = 1
+
+def test_weightafterPost_route( path, expected):
+    global url
+    global status
+    res =''
+    expected_res = ''
+    try:
+        res = str(requests.get(url + path))
+        expected_res = expected
+    except Exception as e:
+        status = 1
+
+    if expected_res in res:
+        status = 1
+
     
 def main():
     
@@ -100,15 +128,15 @@ def main():
 
     #test_rout_weight(url+"/weight",{})
     #Testing Inserting Transaction with 3 containers using POST /weight
-    # timeIn=datetime.now().strftime("%Y%m%d%H%M%S")
-    # test__post_routes(url+"/weight?direction=in&truck=DebugTruck&containers=C1%3APeaches%2BC1%3APeaches%2BC2%3ABananas&weight=390","")
-    # test__post_routes(url+"/weight?direction=none&truck=DebugTruck&containers=C1%3APeaches%2BC2%3ABananas&weight=308","")
-    # test__post_routes(url+"/weight?direction=none&truck=DebugTruck&containers=C2%3ABananas&weight=236","")
-    # test__post_routes(url+"/weight?direction=out&truck=DebugTruck&containers=&weight=150","")
-    # timeOut=datetime.now().strftime("%Y%m%d%H%M%S")
+    timeIn=datetime.now().strftime("%Y%m%d%H%M%S")
+    test_weightPost_route("/weight?direction=in&truck=DebugTruck&containers=C1%3APeaches%2BC1%3APeaches%2BC2%3ABananas&weight=390","<Response [200]>")
+    test_weightPost_route("/weight?direction=none&truck=DebugTruck&containers=C1%3APeaches%2BC2%3ABananas&weight=308","<Response [200]>")
+    test_weightPost_route("/weight?direction=none&truck=DebugTruck&containers=C2%3ABananas&weight=236","<Response [200]>")
+    test_weightPost_route("/weight?direction=out&truck=DebugTruck&containers=&weight=150","<Response [200]>")
+    timeOut=datetime.now().strftime("%Y%m%d%H%M%S")
 
     # # Testing if Transaction was entered successfully
-    # test__get_routes(url+"/weight?from=%s&to=%s&filter=out"%(str(timeIn),str(timeOut)),'{"bruto":"390","containers":"C1,C2","direction":"Out","neto":"210","produces":"Bananas,Peaches"}')
+    test_weightafterPost_route("/weight?from=%s&to=%s&filter=out"%(str(timeIn),str(timeOut)),'{"bruto":"390","containers":"C1,C2","direction":"Out","neto":"210","produces":"Bananas,Peaches"}')
 
 
 
