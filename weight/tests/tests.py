3290c12e0ea95e0162bbb232998644a1dcb7217a
Kate Dvoretsky <kate.dvoretsky@gmail.com>
2020-02-05 14:36:37 +0200

    Templat for test file

--- weight/tests/tests.py
+++ weight/tests/tests.py
@@ -3,22 +3,48 @@ import os
 from datetime import datetime
 
 
-def validate(status, content):
+def validate(status, content, expected):
+    code = 0
     log_info = ""
     if status >= 500 or status <= 599 :
         log_info = "Status:  {}\n Content:\n{} \n Fail.\n".format(status, content)
-        print("1")
+        code = 1 
     else:
-        log_info = "Status:  {}\n Content:\n{} \nInvalid url \n".format(status, content)
-        print("0")
-    return log_info
+        if content in expected:
+            log_info = "Status:  {}\n Content:\n{} \n \n".format(status, content)
+        else:
+            log_info = "Status:  {}\n Content:\n{} \n respone not as expected.\n".format(status, content)
+            code = 1
+    return log_info , code
 
+
+
+
+
+
+
+def test__get_routes(route, expected ):
+    res = requests.get(route)
+    log , code  = validate(res.status_code, res.content, expected)
+    with open("log.log", 'a+') as f:
+        f.write("\n\n" + str(datetime.now()) + "\n\n")
+        f.write("\nRoute:\t" + route + "\nValidation Data: \t" + item + "\n" + log + "\n")
+    if code == 1:
+        print(1)
+        exit()
+
+def test__post_routes():
+    # 
+    pass
+
+
+
+def main():
+    test__get_routes(url+"/weight",{})
+    test_rout_weight(url+"/weight")
+
+    print(0)
 
 url = "http://localhost:{}".format(os.environ['PORT'])
 
-valid_data = ""
-res = requests.get( url + valid_data )
-log = validate(res.status_code, res.content)
-with open("log.log", 'a+') as f:
-    f.write("\n\n" + str(datetime.now()) + "\n\n")
-    f.write("\nRoute:\t" + route + "\nValidation Data: \t" + item + "\n" + log + "\n")
+main()
