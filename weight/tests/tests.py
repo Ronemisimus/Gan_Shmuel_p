83a68345a4766fa42734d8c195c3b697fc798993
Kate Dvoretsky <kate.dvoretsky@gmail.com>
2020-02-05 14:53:17 +0200

    Update test

--- weight/tests/tests.py
+++ weight/tests/tests.py
@@ -10,10 +10,10 @@ def validate(status, content, expected):
         log_info = "Status:  {}\n Content:\n{} \n Fail.\n".format(status, content)
         code = 1 
     else:
-        if content in expected:
+        if expected in content:
             log_info = "Status:  {}\n Content:\n{} \n \n".format(status, content)
         else:
-            log_info = "Status:  {}\n Content:\n{} \n respone not as expected.\n".format(status, content)
+            log_info = "Status:  {}\n Invalid Content:\n{}\n Expected: \n{} \n respone not as expected.\n".format(status, content, expected)
             code = 1
     return log_info , code
 
@@ -28,7 +28,7 @@ def test__get_routes(route, expected ):
     log , code  = validate(res.status_code, res.content, expected)
     with open("log.log", 'a+') as f:
         f.write("\n\n" + str(datetime.now()) + "\n\n")
-        f.write("\nRoute:\t" + route + "\nValidation Data: \t" + item + "\n" + log + "\n")
+        f.write("\nRoute:\t" + route + "\n\n" + log + "\n")
     if code == 1:
         print(1)
         exit()
@@ -40,11 +40,16 @@ def test__post_routes():
 
 
 def main():
-    test__get_routes(url+"/weight",{})
-    test_rout_weight(url+"/weight")
+    test__get_routes(url+"/weight?from=10000303000000&to=30000303000000&f=in",'{"35":{"bruto":"178","containers":"C1,C2","direction":"in","neto":"92","produces":"Apples,Oranges"},"36":{"bruto":"125","containers":"C1","direction":"In","neto":"12","produces":"Test,Tomato"}}')
+    test__get_routes(url+"/weight?from=10000303000000&to=30000303000000",'{"35":{"bruto":"178","containers":"C1,C2","direction":"in","neto":"92","produces":"Apples,Oranges"},"36":{"bruto":"125","containers":"C1","direction":"In","neto":"12","produces":"Test,Tomato"}}')
+
+
+    # test_rout_weight(url+"/weight",{})
 
     print(0)
 
 url = "http://localhost:{}".format(os.environ['PORT'])
+# url = "http://18.194.232.207:8089"
+
 
 main()
