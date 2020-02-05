51cfc7b72bbb374a5cbf97c9832a25d0d82bc6a7
Chris Babitski <chrispushkin@gmail.com>
2020-02-05 14:57:16 +0200

    fixed monitor routes + beautify

--- devops/monitoring/monitor.py
+++ devops/monitoring/monitor.py
@@ -6,33 +6,52 @@ import time
 
 app = Flask(__name__)
 
+printM = '' 
+
 @app.route('/monitor')
 def monitor():
+    global printM
+    #the beginning of the messgae to print is html script that refreshes the page every 30 second
+    printM = '<meta http-equiv="refresh" content="30">'
+
     services = {
         'prd-weight':'8089',
         'stg-weight':'8088',
         'prd-provider':'8087',
-        'stg-provider':'8086',
-        'ci-server':'8085'
+        'stg-provider':'8086'
     }
     paths = ['/','/health']
 
-    #the beginning of the messgae to print is html script that refreshes the page every 30 second
-    printM = '<meta http-equiv="refresh" content="30">'  
+    dev_services = {
+        'ci-server':'8085',
+        'reports-server':'8084'
+    }
+
+    dev_paths = ['/health'] 
+
+    def check(services, service, path):
+        global printM
+        url = 'http://18.194.232.207:'+services[service]+path
+        htmlS = '<a href='+url+'>'+service+path+'</a>'
+        try:   
+            response = requests.get(url) #make sure the request is not made localy
+            if response.status_code == 200:
+                printM += (htmlS+' is up!<br>')
+            else:
+                printM += (htmlS+' returned:'+response.status_code+'<br>')
+        except:
+            printM += (htmlS+' is unreachable<br>')
 
     while True:
         for service in services:
             for path in paths:
-                try:
-                    response = requests.get('http://18.194.232.207:'+services[service]+path) #make sure the request is not made localy
-                    if response.status_code == 200:
-                        printM += (service+path+' is up!<br>')
-                    else:
-                        printM += (service+path+' returned:'+response.status_code+'<br>')
-                except:
-                    printM += (service+path+' is unreachable<br>')
-            
-        printM += time.ctime()
+                check(services, service, path)
+
+        for dev_service in dev_services:
+            for dev_path in dev_paths:
+                check(dev_services, dev_service, dev_path)
+
+        printM += ('<p style="color:#191970"> Last updated: '+time.ctime()+' (UTC)</p>')
         return printM
         
 
