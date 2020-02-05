7e3e4cbf2482d08c6834a9e257bb9d0aade72fd8
vdhyvtcakthdur <eigorek@gmail.com>
2020-02-05 10:58:25 +0200

    dummy tests

--- devops/monitoring/logs/logs.py
+++ devops/monitoring/logs/logs.py
@@ -97,6 +97,7 @@ def test():
         # recipients = ["eigorek@gmail.com"]
     elif pusher in devops_group:
         recipients = ["chrispushkin@gmail.com", pushers_email]
+        # recipients = ["eigorek@gmail.com"]
 
     entry = "Tests and Commits Reports for {}'s Latest Push".format(data['pusher']['name'])
 
