d630435205c0f44265359cefda43ca9cc4c10371
nati-elmaliach <natielmaliach3197@gmail.com>
2020-02-06 19:07:45 +0200

    tesing weight

--- weight/test/tests.py
+++ weight/test/tests.py
@@ -114,7 +114,7 @@ def main():
     test_weightPost_route("/weight?direction=none&truck=DebugTruck&containers=C2%3ABananas&weight=236",'{"37":{"bruto":"390","truck":"DebugTruck"}}' , 2)
     test_weightPost_route("/weight?direction=out&truck=DebugTruck&containers=&weight=150",'{"37":{"bruto":"390","neto":"210","truck":"DebugTruck","truckTara":"150"}}',3 )
 
-    #test_batch_weight("/batch-weight", "file not found or it already in database")
+    test_batch_weight("/batch-weight", "file not found or it already in database" , 4)
 
     print(status)
 
