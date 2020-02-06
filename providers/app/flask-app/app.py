3e905dcb9ea015c9762faf8b8fc7ef98b43bdd59
Aran Shavit <aranshavit@gmail.com>
2020-02-06 10:56:06 +0200

    Test

--- providers/app/flask-app/app.py
+++
@@ -1,8 +0,0 @@
-from app import app
-
-from app import app, db
-from app.models import Provider, Truck, Rate
-
-@app.shell_context_processor
-def make_shell_context():
-    return {'db': db, 'Provider': Provider, 'Truck': Truck, 'Rate': Rate}
--- providers/build.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker build -t provider ./app
-docker-compose up --build
