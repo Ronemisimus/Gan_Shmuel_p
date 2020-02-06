c6e5488c2b51edef2418484c718cec6eb681b032
Aran Shavit <aranshavit@gmail.com>
2020-02-06 11:01:23 +0200

    Test

--- providers/app/flask-app/startup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python3 -m flask run --host=0.0.0.0
-python3 -m flask db init
-# if [ $MIGRATE_DB ]
-# then
-#     flask db migrate -m "chat_rooms" "posts"
-#     flask db upgrade
-# fi
